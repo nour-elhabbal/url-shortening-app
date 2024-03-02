ee48da16214a116e8fa30b5ad6a255ed25c216b7
nourahmed <105996059+nour-elhabbal@users.noreply.github.com>
2024-03-02 15:36:10 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import LinkInput from "./components/LinkInput";
+import { LinksContextProvider } from "./context/linksContext";
+
 import "./styles/App.scss";
 
 function App() {
-  return <div className="App">shortener app</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <LinksContextProvider>
+        <LinkInput />
+      </LinksContextProvider>
+    </div>
+  );
 }
 
 export default App;
