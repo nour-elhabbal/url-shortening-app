0099a65e09aaad7c2b3d9e7a2ae729337ca377bd
nourahmed <105996059+nour-elhabbal@users.noreply.github.com>
2024-03-02 15:33:48 +0000

    Refact: wrapped the "get started" button in an anchor tag

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import CustomButton from "./CustomButton";
 import heroImg from "../assets/images/illustration-working.svg";
 
+import "../styles/Hero.scss";
 const Hero = () => {
   return (
     <div className="hero">
@@ -10,7 +11,14 @@ const Hero = () => {
           Build your brand's recognition and get detailed insights on how your
           links are preforming
         </p>
-        <CustomButton isRounded={true} isWide={true} content={"Get Started"} />
+        <a href="#">
+          {" "}
+          <CustomButton
+            isRounded={true}
+            isWide={true}
+            content={"Get Started"}
+          />
+        </a>
       </div>
       <img src={heroImg} alt="Man sitting on a desk and working" />
     </div>
